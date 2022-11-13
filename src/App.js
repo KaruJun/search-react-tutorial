ef15de63d769f759344baa2485153c0045418491
karube <onedra0310@icloud.com>
2022-11-13 18:17:23 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
 
-function App() {
+function App () {
+  const [users, setUsers] = useState([])
+  const [searchQuery, setSearchQuery] = useState([])
+  const ref = useRef()
+
+  const handleSearch = () => {
+    // フィルタリング機能
+    setSearchQuery(
+      users.filter(user => user.name.toLowerCase().includes(ref.current.value))
+    )
+  }
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => setUsers(data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='main'>
+        <h2>検索アプリ</h2>
+        <input type='text' ref={ref} onChange={() => handleSearch()} />
+        <div className='content'>
+          {searchQuery.map(user => (
+            <div className='box' key={user.id}>
+              <h3>{user.name}</h3>
+              <hr />
+              <p>{user.email}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
